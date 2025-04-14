35d66f2fed34e80566e478d5ab4d98b9cf820c29
03dipak <03dip.vaidya@gmail.com>
2025-04-15 01:41:23 +0530

    required changes

--- lambdas/lambda1/lambda_function.py
+++ lambdas/lambda1/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from automated Lambda!'
+        'body': 'Hello from automated Lambda lambda 1!'
     }
